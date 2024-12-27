3c120e5a43524658b0fd148723dacc715ed1186b
Eric <erizawa2000@gmail.com>
2024-12-26 22:25:37 -0500

    Created executable for the local OSRM server.

---
+++ start_osrm.bat
@@ -0,0 +1,2 @@
+@echo off
+start cmd /K "C:\Users\eriza\Downloads\PPII\Implementacion\BHAVRP\BHAVRP_Java\OSRM_Local_Service\OSRM_HOME\OSRM.v5.24\OSRM.v5.24\osrm-routed.exe C:\Users\eriza\Downloads\PPII\Implementacion\BHAVRP\BHAVRP_Java\OSRM_Local_Service\OSRM_HOME\OSRM.v5.24\OSRM.v5.24\datos\cuba-latest.osrm --algorithm=MLD --port=5000"
